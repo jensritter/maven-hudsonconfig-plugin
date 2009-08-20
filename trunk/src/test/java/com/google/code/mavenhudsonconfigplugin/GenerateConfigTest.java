2b0cc5b701518242528980996998fbc8113ea5c3
jens.ritter <jens.ritter@92f6f2dc-8bc7-11de-a0e8-5982b77fecbe>
2009-08-20 03:14:05 +0000

    komischer checkstyle fehler ...

--- trunk/src/test/java/com/google/code/mavenhudsonconfigplugin/GenerateConfigTest.java
+++ trunk/src/test/java/com/google/code/mavenhudsonconfigplugin/GenerateConfigTest.java
@@ -7,22 +7,22 @@ package com.google.code.mavenhudsonconfigplugin;
  * @author Jens Ritter
  */
 public class GenerateConfigTest extends AbstractBaseJobTest {
-	
-	public void testParameter() throws Exception {
-		GenerateConfig cfg = (GenerateConfig) loadMojo(VALUES,"generate");
-		cfg.defaultValues(null);
-		
-		
-		assertEquals("DESCRIPTION",cfg.description_used);
-		assertEquals("clean install deploy site site:deploy",cfg.goals_used);
-		assertEquals("http://www.google.de/hudson/jobs/google",cfg.hudsonUrl_used);
-		assertEquals("google",cfg.jobName_used);
-		assertEquals("svn",cfg.localpart_used);
-		assertEquals("https://maven-hudsonconfig-plugin.googlecode.com/svn/trunk/",cfg.svnPath_used);		
-	}
 
-	
-	// Parser : 
+    public void testParameter() throws Exception {
+        GenerateConfig cfg = (GenerateConfig) loadMojo(VALUES,"generate");
+        cfg.defaultValues(null);
+
+
+        assertEquals("DESCRIPTION",cfg.description_used);
+        assertEquals("clean install deploy site site:deploy",cfg.goals_used);
+        assertEquals("http://www.google.de/hudson/jobs/google",cfg.hudsonUrl_used);
+        assertEquals("google",cfg.jobName_used);
+        assertEquals("svn",cfg.localpart_used);
+        assertEquals("https://maven-hudsonconfig-plugin.googlecode.com/svn/trunk/",cfg.svnPath_used);
+    }
+
+
+    // Parser : 
     public void testPattern() {
         okThis("http://localhost/test/trunk/");
         okThis("http://localhost/test/trunk");
@@ -38,12 +38,12 @@ public class GenerateConfigTest extends AbstractBaseJobTest {
         String find = info.parseSvnName(path);
         assertTrue(find.equals("test") || find.equals("test-1.0"));
     }
-    
+
     private void failThis(String path) {
         String find = info.parseSvnName(path);
         assertFalse(find.equals("test") || find.equals("test-1.0"));
     }
-    
-    
+
+
 
 }
