028fd444bd777a0fa685748c04e4dbc3b1fef5ce
jens.ritter <jens.ritter@92f6f2dc-8bc7-11de-a0e8-5982b77fecbe>
2009-08-20 03:02:54 +0000

    some bugfixing + junit - tests for maven. ( not so elegant - but better than nothing )

--- trunk/src/test/java/com/google/code/mavenhudsonconfigplugin/GenerateConfigTest.java
+++ trunk/src/test/java/com/google/code/mavenhudsonconfigplugin/GenerateConfigTest.java
@@ -1,16 +1,29 @@
 package com.google.code.mavenhudsonconfigplugin;
 
-import junit.framework.TestCase;
 
 /**
  * The Class GenerateConfigTest.
  * 
  * @author Jens Ritter
  */
-public class GenerateConfigTest extends TestCase {
-    
-    public void testPattern() {
+public class GenerateConfigTest extends AbstractBaseJobTest {
+	
+	public void testParameter() throws Exception {
+		GenerateConfig cfg = (GenerateConfig) loadMojo(VALUES,"generate");
+		cfg.defaultValues(null);
+		
+		
+		assertEquals("DESCRIPTION",cfg.description_used);
+		assertEquals("clean install deploy site site:deploy",cfg.goals_used);
+		assertEquals("http://www.google.de/hudson/jobs/google",cfg.hudsonUrl_used);
+		assertEquals("google",cfg.jobName_used);
+		assertEquals("svn",cfg.localpart_used);
+		assertEquals("https://maven-hudsonconfig-plugin.googlecode.com/svn/trunk/",cfg.svnPath_used);		
+	}
 
+	
+	// Parser : 
+    public void testPattern() {
         okThis("http://localhost/test/trunk/");
         okThis("http://localhost/test/trunk");
         okThis("http://localhost/test/tag/test-1.0");
@@ -19,10 +32,8 @@ public class GenerateConfigTest extends TestCase {
         failThis("http://localhost");
     }
 
-
     GenerateConfig info = new GenerateConfig();
 
-    
     private void okThis(String path) {
         String find = info.parseSvnName(path);
         assertTrue(find.equals("test") || find.equals("test-1.0"));
---
+++ trunk/src/test/java/com/google/code/mavenhudsonconfigplugin/MyProjectStubValues.java
@@ -0,0 +1,12 @@
+package com.google.code.mavenhudsonconfigplugin;
+
+
+public class MyProjectStubValues extends MyProjectStubSimple {
+
+	public MyProjectStubValues()
+    {
+		loadData("src/test/resources/values.xml");
+    }
+	
+
+}
