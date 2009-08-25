ef3889732c1b850670318c0a2d1f0a9f6be2c7cd
jens.ritter <jens.ritter@92f6f2dc-8bc7-11de-a0e8-5982b77fecbe>
2009-08-25 10:05:19 +0000

    more junit

---
+++ src/test/java/com/google/code/mavenhudsonconfigplugin/MyProjectStubOtherScm.java
@@ -0,0 +1,16 @@
+package com.google.code.mavenhudsonconfigplugin;
+
+
+/**
+ * The Class MyProjectStubValues.
+ * @author Jens Ritter
+ */
+public class MyProjectStubOtherScm extends MyProjectStubSimple {
+
+    /**
+     * Instantiates a new my project stub values.
+     */
+    public MyProjectStubOtherScm() {
+        loadData("src/test/resources/other.xml");
+    }
+}
